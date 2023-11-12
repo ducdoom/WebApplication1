5a77c60fa607021c616db8e2df5a5842b368c8bf
Nguyễn Minh Đức <ducdoom@gmail.com>
2023-11-12 12:37:12 +0700

    r

--- WebApplication1/Controllers/HelloController.cs
+++ WebApplication1/Controllers/HelloController.cs
@@ -10,7 +10,7 @@ namespace WebApplication1.Controllers
         [HttpGet]
         public IActionResult Get()
         {
-            return Ok("Hello34561");
+            return Ok("i am your assisstant");
         }
     }
 }
