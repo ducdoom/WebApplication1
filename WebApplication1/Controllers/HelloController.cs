8d4bc5c120132f10803c08528c8b0d7947984d48
ducdoom <100558461+ducdoom@users.noreply.github.com>
2023-11-12 10:02:04 +0700

    Update HelloController.cs

--- WebApplication1/Controllers/HelloController.cs
+++ WebApplication1/Controllers/HelloController.cs
@@ -10,7 +10,7 @@ namespace WebApplication1.Controllers
         [HttpGet]
         public IActionResult Get()
         {
-            return Ok("Hello2");
+            return Ok("Hello345");
         }
     }
 }
